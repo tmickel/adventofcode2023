7ee200ca8fbddc3ba6698b972c5ac4edc7028e2e
Tim Mickel <tim@tmickel.com>
2023-12-06 10:24:43 -0500

    day6 pt 2

--- day6/main.py
+++ day6/main.py
@@ -1,49 +1,15 @@
 import math
-# Time:      7  15   30
-# Distance:  9  40  200
 
-races = [
-    {
-        'time': 7,
-        'distance': 9, 
-    },
-    {
-        'time': 15,
-        'distance': 40, 
-    },
-    {
-        'time': 30,
-        'distance': 200, 
-    }
-]
+race = {
+    'time': 44899691,
+    'distance': 277113618901768
+}
 
-races_input = [
-    {
-        'time': 44,
-        'distance': 277, 
-    },
-    {
-        'time': 89,
-        'distance': 1136, 
-    },
-    {
-        'time': 96,
-        'distance': 1890, 
-    },
-    {
-        'time': 91,
-        'distance': 1768, 
-    }
-]
-
-races_ways = []
-for race in races_input:
-    ways_to_win = 0
-    for hold_time in range(1, race['time']):
-        time_racing = race['time'] - hold_time 
-        speed = hold_time
-        distance = speed * time_racing
-        if distance > race['distance']:
-            ways_to_win += 1
-    races_ways += [ways_to_win]
-print(math.prod(races_ways))
+ways_to_win = 0
+for hold_time in range(1, race['time']):
+    time_racing = race['time'] - hold_time 
+    speed = hold_time
+    distance = speed * time_racing
+    if distance > race['distance']:
+        ways_to_win += 1
+print(ways_to_win)
