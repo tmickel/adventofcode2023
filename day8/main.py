92b92b39cc832bc2829252ac87958174a2fdd04c
Tim Mickel <tim@tmickel.com>
2023-12-08 10:13:41 -0500

    day8: broken pt 2

--- day8/main.py
+++ day8/main.py
@@ -11,17 +11,24 @@ with open("./input.txt") as f:
             right = right.replace(")", "").strip()
             nodes[node_label] = (left, right)
 
-current_node = 'AAA'
+current_nodes = list(filter(lambda k: k.endswith("A"), nodes.keys()))
+
 ip = 0
 count = 0
 while True:
     count+=1
     instruction = instructions[ip]
     if instruction == 'L':
-        current_node = nodes[current_node][0]
+        new_nodes = []
+        for n in list(current_nodes):
+            new_nodes += [nodes[n][0]]
+        current_nodes = new_nodes
     if instruction == 'R':
-        current_node = nodes[current_node][1]
-    if current_node == 'ZZZ':
+        new_nodes = []
+        for n in list(current_nodes):
+            new_nodes += [nodes[n][1]]
+        current_nodes = new_nodes 
+    if all((node.endswith('Z') for node in current_nodes)):
         break
     ip = (ip + 1) % len(instructions)
 print(count)
